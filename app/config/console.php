61c0e4126dfbcc6a37e2846dacafee1c55bcec8b
Sergey88 <Ser-19@ukr.net>
2022-04-24 09:07:49 +0300

    init

---
+++ app/config/console.php
@@ -0,0 +1,29 @@
+<?php
+
+$params = require(__DIR__ . '/params.php');
+
+return [
+    'id' => 'minimal-console',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'app\commands',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'log' => [
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+    'params' => $params,
+];
